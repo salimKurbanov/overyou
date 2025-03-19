cfa50da006940224817c1350ba029d73a3c7f929
Saypi <kurbanovsajp@gmail.com>
2025-03-20 02:10:27 +0300

    upd

---
+++ main/static/main/scripts/script.js
@@ -0,0 +1,17 @@
+const methodList = document.querySelector('.method_list');
+const links = document.querySelectorAll('.link');
+
+const openMethodList = (e) => {
+    e.stopPropagation()
+    links.forEach(link => link.classList.toggle('disabled'));
+    methodList.classList.toggle('active');
+}
+
+const openHeaderAcordeon = () => {
+    const acordeon = document.querySelector('.acordeon')
+    acordeon.classList.toggle('active')
+}
+
+window.addEventListener('click', () => {
+    // methodList.classList.remove('active')
+})
