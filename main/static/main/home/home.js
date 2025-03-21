411dbf8427717616fc7e269e76761b5d421d0df8
Saypi <kurbanovsajp@gmail.com>
2025-03-21 20:23:36 +0300

    upd

--- main/static/main/home/home.js
+++ main/static/main/home/home.js
@@ -2,8 +2,16 @@ const scenaries_items = document.querySelectorAll('.scenaries_item');
 
 const openDescription = (e) => {
     const parentElement = e.target.closest('.scenaries_item')
-    if(!parentElement.classList.contains('active') && !parentElement.classList.contains('e') && !parentElement.classList.contains('g')) {
-        scenaries_items.forEach(item => item.classList.remove('active'))
-        parentElement.classList.add('active')
+    const windowWidth = window.innerWidth
+
+    if(parentElement.classList.contains('e') || parentElement.classList.contains('g')) return
+
+    if(windowWidth > 768) {
+        if(!parentElement.classList.contains('active')) {
+            scenaries_items.forEach(item => item.classList.remove('active'))
+            parentElement.classList.add('active')
+        }
+    } else{
+        parentElement.classList.toggle('active')
     }
 }
