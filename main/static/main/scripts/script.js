451158dd318e5222b78ebccd29b8cf80e8dd2154
Saipykurbanov <kurbanoviliyas@mail.ru>
2025-03-24 00:27:45 +0300

    upd

--- main/static/main/scripts/script.js
+++ main/static/main/scripts/script.js
@@ -4,7 +4,13 @@ const links = document.querySelectorAll('.link');
 const acordeon = document.querySelector('.acordeon')  
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.menu')
+const chat = document.querySelector('.chat_modal')
 const tabContent = document.querySelector('.chat_modal .body .tab_content')
+const chatTab = document.querySelector('.chat_modal .body .tabs')
+const chatBtn = document.getElementById('chat_button')
+const chatForm = document.getElementById('chat_form')
+let type = ''
+
 
 // Функция для остановки всплытия события (stopPropagation)
 const stopPropagation = (e) => {
@@ -48,39 +54,98 @@ const closePanel = () => {
 window.addEventListener('click', () => {
     closeList(); 
     closePanel()
+    closeChat()
 });
+
+const openChat = (e) => {
+    e.stopPropagation()
+    chat.classList.add('open')
+}
+
+const closeChat = () => {
+    if(!chat.classList.contains('open')) return
+
+    chatForm.reset()
+    chat.classList.remove('open')
+    tabContent.querySelectorAll('.form').forEach(item => item.classList.remove('active'))
+    tabContent.style.height = `0px`
+    chatTab.classList.remove('open')
+}
 
 const openChatTabs = (e) => {
     e.stopPropagation()
-    const container = e.currentTarget.closest('.tabs')
     
     const allTabs =  tabContent.querySelectorAll('.form')
-    const openClass = Array.from(allTabs).every(item => item.classList.contains('active'));
-    console.log(allTabs)
-    if(openClass) {
+    const hasActiveTab = Array.from(allTabs).some(tab => tab.classList.contains('active'));
+
+    if(hasActiveTab) {
         allTabs.forEach(item => item.classList.remove('active'))
         tabContent.style.height = `0px`
+        chatBtn.disabled = true;
+        chatForm.reset()
     }
 
-    container.classList.add('open')
+    chatTab.classList.add('open')
 }
 
 const switchChatTab = (e, name) => {
     e.stopPropagation()
-    const container = e.currentTarget.closest('.tabs')
-    if(name == 'teltegram') {
-        container.querySelector('.tab_title h5').textContent = 'Телеграм'
+    if(name == 'telegram') {
+        type = 'telegram'
+        chatTab.querySelector('.tab_title h5').textContent = 'Телеграм'
     } else {
-        container.querySelector('.tab_title h5').textContent = 'Email'
+        type = 'email'
+        chatTab.querySelector('.tab_title h5').textContent = 'Email'
     }
 
     const tab = document.getElementById(`${name}_form`)
 
     tab.classList.add('active')
 
+    chatBtn.disabled = false;
+
     const h = tab.offsetHeight
 
     tabContent.style.height = `${h}px`
 
-    container.classList.remove('open')
+    chatTab.classList.remove('open')
+}
+
+const sendForm = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(chatForm)
+    const data = {type: type}
+
+    if(type == 'telegram') {
+        data['telegram'] = formData.get('telegram')
+        data['name'] = formData.get('name_telegram')
+        data['message'] = formData.get('message_telegram')
+    } else {
+        data['email'] = formData.get('email')
+        data['name'] = formData.get('name_email')
+        data['message'] = formData.get('message_email')
+    }
+
+    // formData.forEach((value, key) => data[key] = value)
+    const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+    console.log(data)
+
+    try {
+        const res = await fetch('api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify(data)
+        })
+
+        if(res.ok) {
+            console.log(res)
+        }
+    } catch (err) {
+        console.log(err)
+    }
 }
