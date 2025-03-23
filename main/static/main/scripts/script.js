fbf3f36d02f7e3dcaf8f01618baf88b4f375a1bd
Saipykurbanov <kurbanoviliyas@mail.ru>
2025-03-23 16:30:50 +0300

    upd

--- main/static/main/scripts/script.js
+++ main/static/main/scripts/script.js
@@ -4,6 +4,7 @@ const links = document.querySelectorAll('.link');
 const acordeon = document.querySelector('.acordeon')  
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.menu')
+const tabContent = document.querySelector('.chat_modal .body .tab_content')
 
 // Функция для остановки всплытия события (stopPropagation)
 const stopPropagation = (e) => {
@@ -48,3 +49,38 @@ window.addEventListener('click', () => {
     closeList(); 
     closePanel()
 });
+
+const openChatTabs = (e) => {
+    e.stopPropagation()
+    const container = e.currentTarget.closest('.tabs')
+    
+    const allTabs =  tabContent.querySelectorAll('.form')
+    const openClass = Array.from(allTabs).every(item => item.classList.contains('active'));
+    console.log(allTabs)
+    if(openClass) {
+        allTabs.forEach(item => item.classList.remove('active'))
+        tabContent.style.height = `0px`
+    }
+
+    container.classList.add('open')
+}
+
+const switchChatTab = (e, name) => {
+    e.stopPropagation()
+    const container = e.currentTarget.closest('.tabs')
+    if(name == 'teltegram') {
+        container.querySelector('.tab_title h5').textContent = 'Телеграм'
+    } else {
+        container.querySelector('.tab_title h5').textContent = 'Email'
+    }
+
+    const tab = document.getElementById(`${name}_form`)
+
+    tab.classList.add('active')
+
+    const h = tab.offsetHeight
+
+    tabContent.style.height = `${h}px`
+
+    container.classList.remove('open')
+}
